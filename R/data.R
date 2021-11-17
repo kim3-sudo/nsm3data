5493e2004150af53a3c08526c1465a9b90752296
Sejin Kim <kim3@kenyon.edu>
2021-11-17 09:59:21 -0500

    Add data.R

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' Mean Interstitial Lengths
+#'
+#' A dataset containing the mean interstitial lengths of 36 different pine trees from 3 different species. The variables are as follows:
+#'
+#' @format A data frame with 36 rows and 2 variables:
+#' \describe{
+#' \item{Lengths}{Length of the tree in millimeters}
+#' \item{Type}{The species of tree (Scotch, BlueSpruce, White)}
+#' }
+"Pine"
