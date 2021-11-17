3441e81aa52ccac2fbccfa510357b1c13b437a38
Sejin Kim <kim3@kenyon.edu>
2021-11-17 11:22:01 -0500

    Add new function for table numbers

---
+++ R/tabnum.R
@@ -0,0 +1,12 @@
+#' Table numbers
+#'
+#' A function to alias all datasets with their corresponding table numbers
+#'
+#' @format No arguments
+#' \describe{
+#' \item{null}{null}
+#' }
+
+tabnum <- function() {
+
+}
