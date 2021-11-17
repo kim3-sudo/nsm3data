622e91cfe7b01596be5c53a6c80f0c9a2d4a6ece
Sejin Kim <kim3@kenyon.edu>
2021-11-17 11:28:19 -0500

    Change library dependency from SparkR to sparklyr

--- R/tabnum.R
+++ R/tabnum.R
@@ -1,6 +1,6 @@
 #' Table numbers
 #'
-#' A function to alias all datasets with their corresponding table numbers
+#' A function to alias all datasets with their corresponding table numbers. All table numbers are prefaced with a `t` (for example, instead of `4.5`, use `t4.5`). Test tables are prefaced with a `s`
 #'
 #' @format No arguments
 #' \describe{
@@ -8,5 +8,6 @@
 #' }
 
 tabnum <- function() {
-
+  library(sparklyr)
+  alias(Pine, s3.1)
 }
