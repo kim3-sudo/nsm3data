2176512bce2aa96bfac83a60bdd2e0f19f032aa2
Sejin Kim <kim3@kenyon.edu>
2021-11-19 09:46:01 -0500

    Add shelterbelts documentation

---
+++ R/shelterbelts.R
@@ -0,0 +1,17 @@
+#' shelterbelts
+#'
+#' Percent reduction in average wind speed at Dambutta, 1980/81 by shelterbelt trees
+#'
+#' @format Six arguments across nine observations
+#' \describe{
+#' \item{Month}{A string with the month}
+#' \item{m20}{Measured at 20 meters of leeward distance from shelterbelt}
+#' \item{m40}{Measured at 40 meters of leeward distance from shelterbelt}
+#' \item{m100}{Measured at 100 meters of leeward distance from shelterbelt}
+#' \item{m150}{Measured at 150 meters of leeward distance from shelterbelt}
+#' \item{m200}{Measured at 200 meters of leeward distance from shelterbelt}
+#' }
+
+load('./data/shelterbelts.rda')
+shelterbelts <- shelterbelts
+save(shelterbelts, file = "./data/shelterbelts.rda")
