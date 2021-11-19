8957245afdefbf36ae68487c9f9c7158b43738c8
Sejin Kim <kim3@kenyon.edu>
2021-11-19 09:33:00 -0500

    Add describe function

---
+++ R/describe.R
@@ -0,0 +1,16 @@
+#' Describe Dataset
+#'
+#' A function to get the quantitative description of a dataset
+#'
+#' @format One argument
+#' \describe{
+#' \item{dataset}{An R data frame object}
+#' }
+
+describe <- function(dataset) {
+  data(dataset)
+  print(paste("A data frame with ", nrow(dataset), " and ", ncol(dataset), "variables: "))
+  for (variable in names(dataset)) {
+    print(paste(variable, " of type ", typeof(variable)))
+  }
+}
