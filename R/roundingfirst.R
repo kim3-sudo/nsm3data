8623f4b7b968cb7a7d2d69913f315515fdb7f44b
Sejin Kim <kim3@kenyon.edu>
2021-11-20 16:56:16 -0500

    Add roundingfirst dataset and sample dataset description file

---
+++ R/roundingfirst.R
@@ -0,0 +1,15 @@
+#' roundingfirst
+#'
+#' Rounding First-Base Times
+#'
+#' @format Seven arguments across twenty-two observations
+#' \describe{
+#' \item{Players}{The player}
+#' \item{roundout}{Player's round out run time in seconds}
+#' \item{roundout.rank}{Player's round out run time rank (by player)}
+#' \item{narrowangle}{Player's narrow angle run time in seconds}
+#' \item{narrowangle.rank}{Player's narrow angle run time rank (by player)}
+#' \item{wideangle}{Player's wide angle run time in seconds}
+#' \item{wideangle.rank}{Player's wide angle run time rank (by player)}
+#' }
+"roundingfirst"
---
+++ R/sampledataset.R
@@ -0,0 +1,11 @@
+#' sample
+#'
+#' Sample dataset description is written here.
+#'
+#' @format M arguments across N observations
+#' \describe{
+#' \item{Variable}{Variable desciption}
+#' \item{Variable}{Variable description}
+#' \item{AnotherVariable}{Add more of these rows as needed}
+#' }
+"sample"
