8ac62862f75a21b2f0f025885aa282d43a6ec4d1
Sejin Kim <kim3@kenyon.edu>
2021-11-29 12:53:50 -0500

    Add psychotherapy data

---
+++ R/psychotherapy.R
@@ -0,0 +1,12 @@
+#' psychoactive
+#'
+#' Raw scores indicating the degree of psychotherapeutic attraction for each experimental condition
+#'
+#' @format 4 arguments across 8 observations
+#' \describe{
+#' \item{control}{The control group}
+#' \item{TR}{Reading}
+#' \item{VTP}{Videotape}
+#' \item{RII}{Group}
+#' }
+"psychoactive"
