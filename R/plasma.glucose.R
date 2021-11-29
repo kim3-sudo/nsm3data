da1280bbf77ece0d10c7e188aea89025141ca524
Sejin Kim <kim3@kenyon.edu>
2021-11-29 12:44:21 -0500

    Change plasma glucose file

---
+++ R/plasma.glucose.R
@@ -0,0 +1,13 @@
+#' plasma.glucose
+#'
+#' Plasma glucose levels in geese after a certain number of hours
+#'
+#' @format M arguments across N observations
+#' \describe{
+#' \item{zero}{Original plasma glucose level}
+#' \item{one}{Plasma glucose level after 1 hour}
+#' \item{four}{Plasma glucose level after 4 hours}
+#' \item{twenty_four}{Plasma glucose level after 24 hours}
+#' \item{ninety_six}{Plasma glucose level after 96 hours}
+#' }
+"plasma.glucose"
