600718c4cf0167d0ef7e7b481ca01a9a8b4fd775
Sejin Kim <kim3@kenyon.edu>
2021-12-01 14:18:52 -0500

    Add data to tabnum function

--- R/tabnum.R
+++ R/tabnum.R
@@ -10,8 +10,12 @@ tabnum <- function() {
   # Please put these in order by table number
   # This will help avoid duplicate aliases
   library(sparklyr)
+  data("psychotherapy")
   alias(psychotherapy, t6.2)
+  data("shelterbelts")
   alias(shelterbelts, t7.7)
+  data("cysticerci")
   alias(cysticerci, t8.3)
+  data("pollution")
   alias(pollution, t8.8)
 }
